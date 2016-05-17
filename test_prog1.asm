215131f50f5abc266dc7d845bc61a8b7541d1807
Shivam Kundan <shivamkundan@hotmail.com>
2016-05-16 23:55:35 -0500

    Update test_prog1.asm
    Added description, formatting.

--- test_prog1.asm
+++ test_prog1.asm
@@ -1,13 +1,17 @@
+#This is a sample assembly program to test the MIPS CPU.
+
+#The relevant register output values are written as inline comments.
+
  
-addi $t0, $zero, 5 #$t0 = 5
-addi $t1, $zero, 6 #$t0 = 6
+addi $t0, $zero, 5       #$t0 = 5
+addi $t1, $zero, 6       #$t0 = 6
 
-add $t3, $t0, $t1 #t3 = 11
+add $t3, $t0, $t1        #t3 = 11
 
-sw $t3, 0($zero) #Mem[0] = 11
+sw $t3, 0($zero)         #Mem[0] = 11
 
-lw $t4, 0($zero) #t4 = 11
+lw $t4, 0($zero)         #t4 = 11
 
-and $t5, $t0, $t4 #t5 = t0 & t4
+and $t5, $t0, $t4        #t5 = t0 & t4
 
 slt $t6, $t0, $t5 
